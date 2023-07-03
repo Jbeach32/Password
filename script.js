8b8481894f6cf1beb30053669b2308fe6cb4698f
Jbeach32 <joshbeach2007@gmail.com>
2023-07-03 16:13:46 -0600

    Behold password generation

--- script.js
+++ script.js
@@ -9,3 +9,62 @@ function writePassword() {
 }
 
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    var length = prompt("Please enter your desired password length (between 8 and 128 characters):");
+    if (length === null || length === "" || isNaN(length)) {
+        alert ("Invalid length, please try again");
+        return "";
+    }
+
+
+length = parseInt(length, 10);
+
+if (length < 8 || length > 128) {
+    alert("Invalid length, please try again");
+    return "";
+}
+
+var uppercase = confirm("Do you want to include uppercase letters?");
+var lowercase = confirm("Do you want to include lowercase letters?");
+var numeric = confirm("Do you want to include numeric characters?");
+var special = confirm("Do you want to include special characters?");
+
+if (!uppercase && !lowercase && !numeric && !special) {
+    alert("Please select at least one password type");
+    return "";
+}
+
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var numericChars = "01234567890";
+var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+var charPool = "";
+
+if (uppercase) {
+    charPool += upperChars;
+}
+
+if (lowercase) {
+    charPool += lowerChars;
+}
+
+if (numeric) {
+    charPool += numericChars;
+}
+
+if (special) {
+    charPool += specialChars;
+}
+
+var password = "";
+
+for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charPool.length);
+    password += charPool[randomIndex];
+}
+
+return password;
+
+}
